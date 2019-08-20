c344300f3ea820b055537d70aa5629d31bca8df6
汪洋 <Wangy40@mingyuanyun.com>
2019-08-20 16:51:41 +0800

    施工ing

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-let A = function () {}
+let A = function () {};
 let a = new A();
-let b = new String('b');
-let c = 1;
-let d = [];
-let e = {};
 
-console.log(a.__proto__ === A.prototype);
-console.log(b.__proto__ === String.prototype);
-console.log(c.__proto__ === Number.prototype);
-console.log(d.__proto__ === Array.prototype);
-console.log(e.__proto__ === Object.prototype);
+console.log(a.__proto__.constructor === A)
+console.log(a.constructor === A)
+console.log(a instanceof A)
+console.log(a.__proto__ instanceof A)
