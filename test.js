1318ee2c3dc7f549188410cec157be3ecb3f8f63
汪洋 <Wangy40@mingyuanyun.com>
2019-08-27 19:23:07 +0800

    新增柯里化文章

--- test.js
+++ test.js
@@ -1,61 +1,6 @@
-let arr = [ 12, 1, 6, 24, 5, 8, 34, 23 ]
-
-function quickSort(arr, left, right) {
-  var len = arr.length,
-    partitionIndex,
-    left = typeof left != 'number' ? 0 : left,
-    right = typeof right != 'number' ? len - 1 : right;
-
-  if (left < right) {
-    partitionIndex = partition(arr, left, right);
-    quickSort(arr, left, partitionIndex - 1);
-    quickSort(arr, partitionIndex + 1, right);
-  }
-  return arr;
+let a = 2;
+switch (a) {
+  case a == 2: 
+    console.log(1)
+    break
 }
-
-function partition(arr, left, right) { // 分区操作
-  var pivot = left, // 设定基准值（pivot）
-    index = pivot + 1;
-  for (var i = index; i <= right; i++) {
-    if (arr[i] < arr[pivot]) {
-      i != index && swap(arr, i, index);
-      index++;
-    }
-  }
-  swap(arr, pivot, index - 1);
-  return index - 1; 
-}
-
-function swap(arr, i, j) {
-  var temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
-}
-
-function partition2(arr, low, high) {
-  let pivot = arr[low];
-  while (low < high) {
-    while (low < high && arr[high] > pivot) {
-      --high;
-    }
-    arr[low] = arr[high];
-    while (low < high && arr[low] <= pivot) {
-      ++low;
-    }
-    arr[high] = arr[low];
-  }
-  arr[low] = pivot;
-  return low;
-}
-
-function quickSort2(arr, low = 0, high = arr.len-1) {
-  if (low < high) {
-    let pivot = partition2(arr, low, high);
-    quickSort2(arr, low, pivot - 1);
-    quickSort2(arr, pivot + 1, high);
-  }
-  return arr;
-}
-
-console.log(quickSort2(arr))
